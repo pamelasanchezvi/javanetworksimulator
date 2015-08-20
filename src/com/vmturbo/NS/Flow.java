937ac5b17b30a49d40e7bd9727e16c2c42e5bb0a
Shangshang Chen <shangshang.chen@vmturbo.com>
2015-08-20 17:53:48 -0400

    updated Flow class

--- src/com/vmturbo/NS/Flow.java
+++ src/com/vmturbo/NS/Flow.java
@@ -1,22 +1,50 @@
 package com.vmturbo.NS;
 
-
 public class Flow {
 
-    public String sourceIP, destIP;
-    public int sourcePort, destPort;
-    public String protocol;
-
-    public int size; //total bytes of the flow
+    String sourceIP, destIP;
+    int sourcePort, destPort;
+    String protocol;
+    int size; //total bytes of the flow
+    int budget; // the budge the flow has, in virtual dollars
 
     public Flow(String sourceIP, String destIP, int sourcePort, int destPort,
-                    String protocol, int size) {
+                    String protocol, int size, int budget) {
         this.sourceIP = sourceIP;
         this.destIP = destIP;
         this.sourcePort = sourcePort;
         this.destPort = destPort;
         this.protocol = protocol;
         this.size = size;
+        this.budget = budget;
+    }
+
+    public String getSourceIP() {
+        return sourceIP;
+    }
+
+    public String getDestIP() {
+        return destIP;
+    }
+
+    public int getSourcePort() {
+        return sourcePort;
+    }
+
+    public int getDestPort() {
+        return destPort;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getBudget() {
+        return budget;
     }
 
 
