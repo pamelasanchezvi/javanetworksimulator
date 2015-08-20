bfb51d2fc720c649952b449ca639586c2d316285
Shangshang Chen <shangshang.chen@vmturbo.com>
2015-08-20 17:10:58 -0400

    preliminary Path class

--- src/com/vmturbo/NS/Path.java
+++ src/com/vmturbo/NS/Path.java
@@ -5,24 +5,23 @@ import java.util.ArrayList;
 public class Path {
 
     Host source;
-    ArrayList<Switch> nodes;
-    Host destination;
+    ArrayList<Switch> switches;
+    Host dest;
 
-    public Path(ArrayList<String> nodes) {
-        this.nodes = nodes;
+    public Path(Host source, Host dest, ArrayList<Switch> switches) {
+        this.switches = switches;
+        this.source = source;
+        this.dest = dest;
     }
 
-    public String getSource() {
-        return this.nodes.get(0);
+    public Host getSource() {
+        return source;
     }
 
-    public String getDest() {
-        return this.nodes.get(nodes.size() - 1);
+    public Host getDest() {
+        return dest;
     }
 
 
-    public static void main(String[] args) {
-        ArrayList<String> path0 = new ArrayList
-        Path path1 = new Path(nodes);
-    }
+
 }
