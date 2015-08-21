c2796b10c59e89346b504f1ae2750cbfd75346e3
Kunal Mahajan <kunal.mahajan@vmturbo.com>
2015-08-21 11:33:57 -0400

    host can be connected to multiple ToR switches

--- src/com/vmturbo/NS/Host.java
+++ src/com/vmturbo/NS/Host.java
@@ -3,6 +3,8 @@
  */
 package com.vmturbo.NS;
 
+import java.util.ArrayList;
+
 /**
  * @author kunal
  *
@@ -10,27 +12,36 @@ package com.vmturbo.NS;
 public class Host {
 
 	String name;
-	ToRSwitch tor; 
+	ArrayList<ToRSwitch> torList; 
 	
-	public Host(String name, ToRSwitch tor){
+	public Host(String name, ArrayList<ToRSwitch> torList){
 		this.name = name;
-		this.tor = tor;
+		this.torList = torList;
 	}
 	
 	public String getName(){
 		return name;
 	}
 	
-	public ToRSwitch getToR(){
-		return tor;
+	public ArrayList<ToRSwitch> getToR(){
+		return torList;
 	}
 	
 	public void setName(String hostname){
 		this.name = hostname;
 	}
 	
-	public void setToRSwitch(ToRSwitch torSwitch){
-		this.tor = torSwitch;
+	public void addtorSwitch(ToRSwitch torSwitch){
+		if(torList.contains(torSwitch)){
+			return;
+		}
+		torList.add(torSwitch);
+	}
+	
+	public void removetorSwitch(ToRSwitch torSwitch){
+		if(torList.contains(torSwitch)){
+			torList.remove(torSwitch);
+		}
 	}
 	
 }
