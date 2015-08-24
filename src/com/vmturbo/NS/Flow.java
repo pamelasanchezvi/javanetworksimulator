e4bd326f4e396e77dd70c567dd974eb0d10360cb
Shangshang Chen <shangshang.chen@vmturbo.com>
2015-08-24 15:56:04 -0400

    Flow class: updated toString method; changed "bandwidth" to type "double"

--- src/com/vmturbo/NS/Flow.java
+++ src/com/vmturbo/NS/Flow.java
@@ -7,11 +7,11 @@ public class Flow {
 
     Host source, dest;
     int start, duration; //in seconds
-    int bandwidth; //total bytes of the flow
+    double bandwidth; //bandwidth of the flow
     int budget; // the budge the flow has, in virtual dollars
 
-    //constructor: assuming infinite budget right now
-    public Flow(Host source, Host dest, int start, int duration, int bandwidth /*, int budget*/) {
+    //constructor: assuming infinite budget for now
+    public Flow(Host source, Host dest, int start, int duration, double bandwidth /*, int budget*/) {
         this.source = source;
         this.dest = dest;
         this.start = start;
@@ -36,11 +36,12 @@ public class Flow {
         return duration;
     }
 
+    //is this method meaningful?
     public int getEnd() {
         return start + duration;
     }
 
-    public int getBandwidth() {
+    public double getBandwidth() {
         return bandwidth;
     }
 
@@ -51,29 +52,20 @@ public class Flow {
     @Override
     public String toString() {
         return ("flow: "
-                + source.getName()
-                + " -> "
-                + dest.getName()
-                + "; "
-                + getStart()
-                + "s -> "
-                + getEnd()
-                + "s; "
-                + bandwidth
-                + " bytes; "
-                /**
-                + budget
-                + " $;" 
-                */
+                + source.getName() + " -> " + dest.getName() + "; "
+                + "starts: " + getStart() + "s; "
+                + "duration: " + getDuration() + "s; "
+                + bandwidth + " Gb/s; "
+                //+ budget + " $;" 
                 + "\n");
-            }
+    }
 
     /** for testing purpose
     public static void main(String[] args) {
         Host h1 = new Host("h1");
         Host h2 = new Host("h2");
-        Flow f = new Flow(h1, h2, 0, 20, 1000, 100);
-        
+        Flow f = new Flow(h1, h2, 0, 20, 0.5);
+        System.out.println(f);
     }
     */
 }
