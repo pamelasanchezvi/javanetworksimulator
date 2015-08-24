4b411ef563efef671f94c8f101ec860cec5e89ac
Kunal Mahajan <kunal.mahajan@vmturbo.com>
2015-08-24 11:51:01 -0400

    incomplete draft of flowQueueSetup

--- src/com/vmturbo/NS/FlowQueueSetup.java
+++ src/com/vmturbo/NS/FlowQueueSetup.java
@@ -3,6 +3,9 @@
  */
 package com.vmturbo.NS;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -24,6 +27,41 @@ public class FlowQueueSetup {
 	 */
 	public void populateQueue(){
 		
+		BufferedReader reader;
+		String readLine;
+		String[] flowData;
+		Host source;
+		Host destination;
+		int start;
+		int duration;
+		int size;
+		
+		try{
+			reader = new BufferedReader(new FileReader(queueFileName)); 
+			// read line by line
+			while((readLine = reader.readLine()) != null){
+				System.out.println(readLine);
+				readLine.trim();
+				flowData = readLine.split(";");
+
+				if(flowData.length == 5){
+					// construct Flow object
+					start = Integer.parseInt(flowData[0]);
+					duration = Integer.parseInt(flowData[1]);
+					source = getHost(flowData[2]);		// method should be implemented in TopologySetup
+					destination = getHost(flowData[3]);
+					size = Integer.parseInt(flowData[4]);
+					Flow flowToAdd = new Flow(source, destination, start, duration, size);
+
+					// add Flow object to queue
+					addFlow(flowToAdd);
+				} else {
+					throw new IllegalArgumentException();
+				}
+			}
+		} catch (IOException ioe){
+			System.err.println("Caught IOException: " + ioe.getMessage());
+		}
 	}
 	
 	public String getQueueFileName(){
