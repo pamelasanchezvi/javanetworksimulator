1a53d42696f0933d1421eee460e91bdf122ffafd
Shangshang Chen <shangshang.chen@vmturbo.com>
2015-08-24 16:21:52 -0400

    Path class: testing in progress

--- src/com/vmturbo/NS/Path.java
+++ src/com/vmturbo/NS/Path.java
@@ -11,6 +11,7 @@ public class Path {
     ArrayList<Link> links;
     Host dest;
 
+    //constructor
     public Path(Host source, Host dest, ArrayList<Link> links) {
         this.links = links;
         this.source = source;
@@ -42,13 +43,14 @@ public class Path {
 
 
     /**
+     * used for economic placement
      * assuming we are not doing multi-threading, no need for locks
      * @param flow
      * @return -1 if path can't satisfy flow demand, an integer quote otherwise
      */
     public int getQuote(Flow flow) {
         //"demand" is the bandwidth demand of the flow
-        double demand = (double)flow.getBandwidth() / (double)flow.getDuration();
+        double demand = flow.getBandwidth();
 
         //quote of the path is the sum of link prices
         //link price is calculated as 1/(1-U)^2, U is percentage utilized of the link
@@ -59,17 +61,20 @@ public class Path {
                 return -1;
             }
             double percentage = (demand + link.getUtilization()) / link.getCapacity();
-            quote += 1 / Math.pow(1 - percentage, 2);
+            double linkPrice = 1 / Math.pow(1 - percentage, 2);
+            quote += linkPrice;
         }
         return quote;
     }
 
+
     /**
+     * used for economic placement
      * @param flow
      * @return 0 if successful, -1 unsuccessful
      */
     public int placeFlow(Flow flow) {
-        double demand = (double)flow.getBandwidth() / (double)flow.getDuration();
+        double demand = flow.getBandwidth();
         for (Link link : links) {
             //for now, just push everything into the link
             link.setUtilization(Math.min(link.getCapacity(), link.getUtilization() + demand));
@@ -78,18 +83,19 @@ public class Path {
     }
 
     /**
+     * used for economic placement
      * @param flow
      * @return 0 if successful, -1 unsuccessful
      */
     public int removeFlow(Flow flow) {
-        double demand = (double)flow.getBandwidth() / (double)flow.getDuration();
+        double demand = flow.getBandwidth();
         for (Link link : links) {
             link.setUtilization(Math.max(0, link.getUtilization() - demand));
         }
         return 0;
     }
 
-    /** testing
+    ///** testing
     public static void main(String[] args) {
 
         Host a = new Host("a");
@@ -103,11 +109,11 @@ public class Path {
         SpineSwitch spine = new SpineSwitch("spine");
         spine.addtorSwitch(tor1);
         spine.addtorSwitch(tor2);
-        Link l1 = new Link(a, tor1, 0, 0);
-        Link l2 = new Link(tor1, b, 0, 0);
-        Link l3 = new Link(tor1, spine, 0, 0);
-        Link l4 = new Link(spine, tor2, 0, 0);
-        Link l5 = new Link(tor2, c, 0, 0);
+        Link l1 = new Link(a, tor1, 1, 0.5);
+        Link l2 = new Link(tor1, b, 1, 0.5);
+        Link l3 = new Link(tor1, spine, 10, 2);
+        Link l4 = new Link(spine, tor2, 10, 2);
+        Link l5 = new Link(tor2, c, 1, 0.5);
 
 
 
@@ -127,8 +133,20 @@ public class Path {
         ArrayList<Path> paths = new ArrayList<Path>();
         paths.add(p1);
         paths.add(p2);
+        //test toString() 
         System.out.println(paths);
+
+        //test getQuote()
+        Flow f1 = new Flow(a, b, 0, 10, 0.1); //should print 12
+        System.out.println(p1.getQuote(f1));
+        Flow f2 = new Flow(a, b, 0, 10, 0.4); //should print 200
+        System.out.println(p1.getQuote(f2));
+        Flow f3 = new Flow(a, b, 0, 10, 0.5); //should print -1
+        System.out.println(p1.getQuote(f3));
+        Flow f4 = new Flow(a, c, 0, 10, 0.1);
+        System.out.println(p2.getQuote(f4));
+
     }
-    */
+    //*/
 
 }
